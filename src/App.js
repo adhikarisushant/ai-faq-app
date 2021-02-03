37737b31894a7f52549a30b64f925b0e53e36aef
Sushant <sushant.adhikari.619@gmail.com>
2021-02-03 09:55:53 +0545

    FAQ UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Navbar from './Navbar';
+import Faq from './Faq';
 
 const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
+      <Faq />
     </ChakraProvider>
   )
 }
 
-export default App
+export default App;
