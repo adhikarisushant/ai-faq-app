3c5649a9037012144a36eb8f528ce22b80b5f02c
Sushant <sushant.adhikari.619@gmail.com>
2021-02-03 19:16:17 +0545

    React Alan AI integration, sending FAQ voice data to React APp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 import Navbar from './Navbar';
 import Faq from './Faq';
+import alanBtn from "@alan-ai/alan-sdk-web";
+import { scroller } from 'react-scroll';
 
 const App = () => {
+  const alanBtnInstance = useRef(null);
+  const [index, setIndex] = useState(null);
+
+  useEffect(() => {
+      if (!alanBtnInstance.current) {
+          alanBtnInstance.current = alanBtn({
+              key: '26d921da982620200c38b0b00d27a92c2e956eca572e1d8b807a3e2338fdd0dc/stage',
+              onCommand: (commandData) => {
+                  if (commandData.command === 'gotoFaq') {
+                        scroller.scrollTo(`accordion-item-${commandData.faqId}`, {
+                          duration: 800,
+                          delay: 0,
+                          smooth: 'easeInOutQuart'
+                        })
+                        setIndex(commandData.faqId - 1)
+                  }
+                  }
+          });
+      }
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Navbar />
-      <Faq />
+      <Faq index={index} setIndex={setIndex} />
     </ChakraProvider>
   )
 }
--- src/Faq.js
+++ src/Faq.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { Box, Text, Flex, Heading, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from '@chakra-ui/react';
 import FAQ_LIST from './faq.json';
 
-const Faq = () => {
+const Faq = ({ index, setIndex }) => {
     return (
         <flex direction="column" p="4">
             <Box mb="8">
                 <Heading size="md">Frequently Asked Questions</Heading>
             </Box>
-            <Accordion>
+            <Accordion allowToggle index={index}>
                 {
                     FAQ_LIST.map( faq => (
-                        <AccordionItem key={faq.id}>
-                            <AccordionButton>
+                        <AccordionItem key={faq.id} name={`accordion-item-${faq.id}`}>
+                            <AccordionButton onClick={() => setIndex(faq.id -1)}>
                                 <Box flex="1" textAlign="left">
                                     <Text fontWeight="semibold">{faq.question}</Text>     
                                 </Box>
